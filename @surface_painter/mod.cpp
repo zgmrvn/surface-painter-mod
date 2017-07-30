3d7d897baf9b8cf0dbba130f1bbdabc89c7a161f
zgmrvn <zgmrvn+github@gmail.com>
2017-07-30 13:55:40 +0200

    logo + mod description

---
+++ @surface_painter/mod.cpp
@@ -0,0 +1,12 @@
+name = "Suface Painter 0.1.0";
+picture = "icon.paa";
+actionName = "GitHub";
+action = "https://github.com/zgmrvn/surface-painter-mod";
+description = "Suface Painter - Version 0.1.0";
+logo = "icon.paa";
+logoOver = "icon.paa";
+tooltip = "Surface Painter";
+tooltipOwned = "Surface Painter Owned";
+overview = "Surface Painter, a tool for map makers.";
+author = "zgmrvn";
+overviewPicture = "icon.paa";
